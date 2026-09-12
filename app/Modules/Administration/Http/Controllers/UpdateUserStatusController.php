028b5bedd96898e674b31afb4846bd58661a2a3b
jan-brand <jan.brand@vdb.schule>
2026-09-12 22:16:04 +0200

    feat(administration): gate account status endpoint

--- app/Modules/Administration/Http/Controllers/UpdateUserStatusController.php
+++ app/Modules/Administration/Http/Controllers/UpdateUserStatusController.php
@@ -5,6 +5,7 @@ namespace App\Modules\Administration\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Modules\Administration\Actions\DisableUserAction;
 use App\Modules\Administration\Actions\ReactivateUserAction;
+use App\Modules\Administration\Enums\AdministrationCapability;
 use App\Modules\Administration\Exceptions\AdministrationActionRejected;
 use App\Modules\Administration\Support\AdministrationAccess;
 use App\Modules\Identity\Models\User;
@@ -25,7 +26,10 @@ final class UpdateUserStatusController extends Controller
 
         abort_unless(
             $actor instanceof User
-            && $access->canManage($actor),
+            && $access->allowsCapability(
+                $actor,
+                AdministrationCapability::UserStatusManage,
+            ),
             403,
         );
 
